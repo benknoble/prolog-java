a52c3c340d434736824a312bd38440387f3ce709
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-09-15 11:07:20 -0400

    add maven setup

---
+++ src/main/java/edu/unc/cs/comp524/parsers/App.java
@@ -0,0 +1,13 @@
+package edu.unc.cs.comp524.parsers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
