98f9b42780368239385a8117a21743dfc0d8afe9
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-09-16 21:13:45 -0400

    import antlr runtime

---
+++ src/main/java/edu/unc/cs/comp524/parsers/PrintFacts.java
@@ -0,0 +1,12 @@
+package edu.unc.cs.comp524.parsers;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class PrintFacts
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
