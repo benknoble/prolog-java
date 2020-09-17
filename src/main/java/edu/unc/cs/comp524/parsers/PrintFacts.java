8e6360de03382063711e84993e3a09d744a14ac9
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-09-17 08:37:48 -0400

    print the tree

--- src/main/java/edu/unc/cs/comp524/parsers/PrintFacts.java
+++ src/main/java/edu/unc/cs/comp524/parsers/PrintFacts.java
@@ -1,12 +1,24 @@
 package edu.unc.cs.comp524.parsers;
 
+import java.io.*;
+
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
 public class PrintFacts
 {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
-    }
+  public static void main(String[] args)
+    throws IOException
+  {
+    // create a CharStream that reads from standard input
+    CharStream input = CharStreams.fromStream(System.in);
+    // create a lexer that feeds off of input CharStream
+    PrologLexer lexer = new PrologLexer(input);
+    // create a buffer of tokens pulled from the lexer
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    // create a parser that feeds off the tokens buffer
+    PrologParser parser = new PrologParser(tokens);
+    ParseTree tree = parser.p_text(); // begin parsing at init rule
+    System.out.println(tree.toStringTree(parser)); // print LISP-style tree
+  }
 }
