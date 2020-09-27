5c20195efbeadf4fd7533d2e5d24bae494166ef7
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-09-27 19:04:18 -0400

    model: add comments

---
+++ src/main/java/edu/unc/cs/comp524/parsers/prolog/AComment.java
@@ -0,0 +1,30 @@
+package edu.unc.cs.comp524.parsers.prolog;
+
+import java.util.*;
+import java.util.stream.*;
+
+import org.antlr.v4.runtime.*;
+
+public class AComment implements Comment {
+
+  private final String text;
+
+  public AComment(final String s) {
+    text = s;
+  }
+
+  public AComment(final Token t) {
+    this(t.getText());
+  }
+
+  public AComment(final List<Token> ts) {
+    this(ts.stream()
+        .map(t -> t.getText())
+        .collect(Collectors.joining("\n")));
+  }
+
+  @Override
+  public String text() {
+    return text;
+  }
+}
---
+++ src/main/java/edu/unc/cs/comp524/parsers/prolog/Comment.java
@@ -0,0 +1,5 @@
+package edu.unc.cs.comp524.parsers.prolog;
+
+public interface Comment {
+  public String text();
+}
