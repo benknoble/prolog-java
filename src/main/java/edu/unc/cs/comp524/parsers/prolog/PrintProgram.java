7f1c61b62744ba6b537a9c9f45e4c9bee2bb4329
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-09-29 20:16:15 -0400

    add a program printer

---
+++ src/main/java/edu/unc/cs/comp524/parsers/prolog/PrintProgram.java
@@ -0,0 +1,23 @@
+package edu.unc.cs.comp524.parsers.prolog;
+
+import java.io.*;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class PrintProgram {
+  public static void main(String[] args)
+    throws IOException
+  {
+    var input = CharStreams.fromStream(System.in);
+    var lexer = new PrologLexer(input);
+    var tokens = new CommonTokenStream(lexer);
+    var parser = new PrologParser(tokens);
+    var tree = parser.p_text();
+
+    var collector = new RelationCollectorListener(tokens, parser);
+    ParseTreeWalker.DEFAULT.walk(collector, tree);
+    System.out.println("program tree");
+    System.out.println(collector.program());
+  }
+}
