294e657a57d22c2f2d8f9ee26175c3cca0abc9a9
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-09-30 12:35:57 -0400

    utils: add a method to join lists together
    It probably works on any Collection<T>, but oh well

--- src/main/java/edu/unc/cs/comp524/parsers/prolog/ParserUtils.java
+++ src/main/java/edu/unc/cs/comp524/parsers/prolog/ParserUtils.java
@@ -1,5 +1,8 @@
 package edu.unc.cs.comp524.parsers.prolog;
 
+import java.util.*;
+import java.util.stream.*;
+
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import org.antlr.v4.runtime.tree.pattern.*;
@@ -37,5 +40,12 @@ public class ParserUtils {
 
   public static int countLines(final Token t) {
     return t.getText().split("\n").length - 1;
+  }
+
+  public static <T> List<T> join(List<T>... lists) {
+    return
+      Stream.of(lists)
+      .flatMap(Collection::stream)
+      .collect(Collectors.toList());
   }
 }
