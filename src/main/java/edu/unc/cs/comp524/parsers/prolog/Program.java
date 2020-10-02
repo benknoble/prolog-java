26949b11a0a53b05d0b7267efc02da5fdb293380
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-10-02 11:18:33 -0400

    Program: refactor duplicate code
    rules, relations, invocations were all common code patterns, so I
    extracted them

--- src/main/java/edu/unc/cs/comp524/parsers/prolog/Program.java
+++ src/main/java/edu/unc/cs/comp524/parsers/prolog/Program.java
@@ -22,10 +22,7 @@ public interface Program {
 
   public default List<Relation> relationsWithComments() {
     return
-      clauses()
-      .values()
-      .stream()
-      .flatMap(Collection::stream)
+      relations()
       .filter(r -> r.comment().isPresent())
       .collect(Collectors.toList());
   }
@@ -35,10 +32,7 @@ public interface Program {
     if (clauses.isEmpty())
       return false;
     return
-      clauses
-      .stream()
-      .filter(c -> c instanceof Rule)
-      .map(f -> (Rule)f)
+      rules(clauses.stream())
       .anyMatch(r -> r.rhs().stream().anyMatch(ri -> ri.isInvocationOf(r)));
   }
 
@@ -47,44 +41,49 @@ public interface Program {
     if (clauses.isEmpty())
       return false;
     return
-      clauses
-      .stream()
-      .filter(c -> c instanceof Rule)
-      .map(f -> (Rule)f)
-      .flatMap(r -> r.rhs().stream())
+      invocations(clauses.stream())
       .map(RuleInvocation::name)
       .anyMatch(this::isRecursive);
   }
 
   public default List<RuleInvocation> undefined() {
-    var invocations =
-      clauses()
-      .values()
-      .stream()
-      .flatMap(Collection::stream)
-      .filter(c -> c instanceof Rule)
-      .map(f -> (Rule)f)
-      .flatMap(r -> r.rhs().stream());
-    var defined =
-      clauses()
-      .values()
-      .stream()
-      .flatMap(Collection::stream)
-      /* collection necessary here to avoid issues with re-using a consumed
-       * stream
-       *
-       * each stream can be consumed exactly once, and we need to use this one
-       * many times (defined is captured in the isInvocationOfAnyRule closure)
-       *
-       * so we collect it and stream it anew at each use
-       */
-      .collect(Collectors.toList());
+    var invocations = invocations(relations());
+    /* collection necessary here to avoid issues with re-using a consumed
+     * stream
+     *
+     * each stream can be consumed exactly once, and we need to use this one
+     * many times (defined is captured in the isInvocationOfAnyRule closure)
+     *
+     * so we collect it and stream it anew at each use
+     */
+    var defined = relations().collect(Collectors.toList());
     Predicate<RuleInvocation> isInvocationOfAnyRule =
       ri -> defined.stream().anyMatch(r -> ri.isInvocationOf(r));
     return
       invocations
       .filter(isInvocationOfAnyRule.negate())
       .collect(Collectors.toList());
+  }
+
+  private static Stream<Rule> rules(Stream<Relation> relations) {
+    return
+      relations
+      .filter(c -> c instanceof Rule)
+      .map(r -> (Rule)r);
+  }
+
+  private static Stream<RuleInvocation> invocations(Stream<Relation> relations) {
+    return
+      rules(relations)
+      .flatMap(r -> r.rhs().stream());
+  }
+
+  private Stream<Relation> relations() {
+    return
+      clauses()
+      .values()
+      .stream()
+      .flatMap(Collection::stream);
   }
 
 }
