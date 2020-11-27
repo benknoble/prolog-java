65f45feda424a62684e1335a31294a231fda042d
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-11-27 17:30:26 -0500

    fixup! docs: document Fact & Rule

--- src/main/java/edu/unc/cs/comp524/parsers/prolog/Fact.java
+++ src/main/java/edu/unc/cs/comp524/parsers/prolog/Fact.java
@@ -1,6 +1,6 @@
 package edu.unc.cs.comp524.parsers.prolog;
 
 /**
- * The simplest kind of relation: a fact.
+ * The simplest kind of {@link Relation}: a fact.
  */
 public interface Fact extends Relation {}
