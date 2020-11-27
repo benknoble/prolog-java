78c7655ddc81c051f0f3014f7117f13b378a01ed
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-11-27 17:13:31 -0500

    docs: document Comment & AComment

--- src/main/java/edu/unc/cs/comp524/parsers/prolog/AComment.java
+++ src/main/java/edu/unc/cs/comp524/parsers/prolog/AComment.java
@@ -5,6 +5,9 @@ import java.util.stream.*;
 
 import org.antlr.v4.runtime.*;
 
+/**
+ * The default concrete {@link Comment} implementation.
+ */
 public class AComment implements Comment {
 
   private final String text;
@@ -13,10 +16,20 @@ public class AComment implements Comment {
     text = s;
   }
 
+  /**
+   * Constructor for use with {@link PrologParser}-related objects.
+   *
+   * @param t Token from which to derive {@link #text}
+   */
   public AComment(final Token t) {
     this(t.getText());
   }
 
+  /**
+   * Constructor for use with {@link PrologParser}-related objects.
+   *
+   * @param ts Tokens from which to derive {@link #text}
+   */
   public AComment(final List<Token> ts) {
     this(ts.stream()
         .map(t -> t.getText())
--- src/main/java/edu/unc/cs/comp524/parsers/prolog/Comment.java
+++ src/main/java/edu/unc/cs/comp524/parsers/prolog/Comment.java
@@ -1,5 +1,18 @@
 package edu.unc.cs.comp524.parsers.prolog;
 
+/**
+ * A source-level comment
+ * <p>
+ * Prolog has two kinds of comments:
+ * <p>
+ * {@code % line comments}
+ * <p>
+ * and multi-line java-and-C-like comments
+ */
 public interface Comment {
+
+  /**
+   * The comment text
+   */
   public String text();
 }
