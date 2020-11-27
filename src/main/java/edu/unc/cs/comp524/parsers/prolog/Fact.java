7198997f0a6f8f17b74939b1983eb46272b4d5fa
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-11-27 17:00:16 -0500

    docs: document Fact & Rule

--- src/main/java/edu/unc/cs/comp524/parsers/prolog/Fact.java
+++ src/main/java/edu/unc/cs/comp524/parsers/prolog/Fact.java
@@ -1,3 +1,6 @@
 package edu.unc.cs.comp524.parsers.prolog;
 
+/**
+ * The simplest kind of relation: a fact.
+ */
 public interface Fact extends Relation {}
--- src/main/java/edu/unc/cs/comp524/parsers/prolog/Rule.java
+++ src/main/java/edu/unc/cs/comp524/parsers/prolog/Rule.java
@@ -2,6 +2,15 @@ package edu.unc.cs.comp524.parsers.prolog;
 
 import java.util.*;
 
+/**
+ * A more complicated {@link Relation} that has sub-rules ({@link
+ * RuleInvocation}s) on the right-hand-side ({@link #rhs}).
+ */
 public interface Rule extends Relation {
+
+  /**
+   * The sub-rules or sub-clauses that make up the right-hand side of a {@link
+   * Relation}
+   */
   public List<RuleInvocation> rhs();
 }
