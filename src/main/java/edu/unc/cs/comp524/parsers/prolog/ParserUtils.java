713714997cf43a478b37fd0477dcb1e3b8186ee0
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-11-28 16:27:33 -0500

    docs: document ParserUtils

--- src/main/java/edu/unc/cs/comp524/parsers/prolog/ParserUtils.java
+++ src/main/java/edu/unc/cs/comp524/parsers/prolog/ParserUtils.java
@@ -7,35 +7,59 @@ import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import org.antlr.v4.runtime.tree.pattern.*;
 
+/**
+ * Static utilities for the package
+ */
 public class ParserUtils {
+
+  /**
+   * A {@link ParseTreePattern} that matches fact definitions.
+   */
   public static final ParseTreePattern factPattern(PrologParser parser) {
     return parser.compileParseTreePattern(
         "<atom>(<termlist>).",
         PrologParser.RULE_clause);
   }
 
+  /**
+   * A {@link ParseTreePattern} that matches rule definitions.
+   */
   public static final ParseTreePattern rulePattern(PrologParser parser) {
     return parser.compileParseTreePattern(
         "<atom>(<termlist>) :- <term>.",
         PrologParser.RULE_clause);
   }
 
+  /**
+   * A {@link ParseTreePattern} that matches rule definitions with no arguments.
+   */
   public static final ParseTreePattern rule0Pattern(PrologParser parser) {
     return parser.compileParseTreePattern(
         "<atom> :- <term>.",
         PrologParser.RULE_clause);
   }
 
+  /**
+   * A {@link ParseTreePattern} that matches rule invocations.
+   */
   public static final ParseTreePattern invocationPattern(PrologParser parser) {
     return parser.compileParseTreePattern(
         "<atom>(<termlist>)",
         PrologParser.RULE_base_term);
   }
 
+  /**
+   * The number of lines in a token's {@link Token#getText}.
+   */
   public static int countLines(final Token t) {
     return t.getText().split("\n").length - 1;
   }
 
+  /**
+   * Concatentates all of the lists.
+   * <p>
+   * Would be written as {@code join = reduce (++) []} in pseudo-Haskell
+   */
   public static <T> List<T> join(List<T>... lists) {
     return
       Stream.of(lists)
