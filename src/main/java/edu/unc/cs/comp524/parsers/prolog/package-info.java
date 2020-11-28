e57cd301874d2ab3351bc666baa84c57a37c1024
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-11-28 16:48:16 -0500

    docs: add some information about getting started

--- src/main/java/edu/unc/cs/comp524/parsers/prolog/package-info.java
+++ src/main/java/edu/unc/cs/comp524/parsers/prolog/package-info.java
@@ -1,5 +1,12 @@
 /**
  * Package holding prolog-parser code built for Comp 524 taught at the
  * University of North Carolina Chapel Hill
+ *<p>
+ * The gateway should be considered {@link PrologParser}, {@link
+ * RelationCollectorListener}, and {@link Program}. Study the sample programs to
+ * see how they can be used together.
+ *<p>
+ * Then, write your own programs that consume {@link Program} to test for
+ * certain aspects of the program-structure.
  */
 package edu.unc.cs.comp524.parsers.prolog;
