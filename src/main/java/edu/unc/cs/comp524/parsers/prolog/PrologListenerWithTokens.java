72ec43515ed0809eece8bc1537893a70e2018068
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-11-28 16:37:43 -0500

    docs: document PrologListenerWithTokens

--- src/main/java/edu/unc/cs/comp524/parsers/prolog/PrologListenerWithTokens.java
+++ src/main/java/edu/unc/cs/comp524/parsers/prolog/PrologListenerWithTokens.java
@@ -4,19 +4,33 @@ import java.util.*;
 
 import org.antlr.v4.runtime.*;
 
+/**
+ * A {@link PrologListener} that saves the {@link BufferedTokenStream} of tokens
+ * for access during the tree-walk.
+ */
 public class PrologListenerWithTokens extends PrologBaseListener {
+  /**
+   * The tokens stream
+   */
   protected final BufferedTokenStream tokens;
 
   public PrologListenerWithTokens(BufferedTokenStream tokens) {
     this.tokens = tokens;
   }
 
+  /**
+   * The {@link Token} nodes of comments to the left of the givent context
+   */
   protected final List<Token> commentsToLeft(ParserRuleContext ctx) {
     return tokens.getHiddenTokensToLeft(
         ctx.getStart().getTokenIndex(),
         PrologLexer.COMMENTCH);
   }
 
+  /**
+   * True iff the comment tokens end on the line before the start of the given
+   * context.
+   */
   protected final boolean areTokensOnNode(ParserRuleContext ctx, List<Token> tokens) {
     int nodeLine = ctx.getStart().getLine();
     Token lastToken = tokens.get(tokens.size()-1);
